93afa1faa1780aba31a88dd7bdf23ddcd4bbf34c
2027839379 <147675652+2027839379@users.noreply.github.com>
2024-01-18 20:26:50 +0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,15 @@
+import nonebot
+from nonebot.adapters.onebot.v11 import Adapter
+
+nonebot.init(debug=True)
+
+app = nonebot.get_asgi()
+
+driver = nonebot.get_driver()
+driver.register_adapter(Adapter)
+
+nonebot.load_builtin_plugins("echo")
+nonebot.load_plugin("nonebot_plugin_gocqhttpQQ")
+
+if __name__ == "__main__":
+    nonebot.run(app="__main__:app")
